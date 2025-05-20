2da3b45eeaaa4bcad83ab2248216a3750177931c
Damián Pumar <damianpumar@gmail.com>
2025-05-20 09:52:47 +0200

    fix: update import syntax for package.json in Vite config files

--- store/vite.config.ts
+++ store/vite.config.ts
@@ -3,7 +3,7 @@ import { federation } from '@module-federation/vite';
 
 import react from '@vitejs/plugin-react';
 
-import packageJson from './package.json' assert { type: 'json' };
+import packageJson from './package.json' with { type: 'json' };
 import { defineCommonConfig } from 'react-microfrontend-common/vite';
 
 export default defineConfig(({ mode }) => {
