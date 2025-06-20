3387edebdb109d95b5b05ca571f237a0634e56cc
Damián Pumar <damianpumar@gmail.com>
2025-06-20 16:27:02 +0200

    fix: update import path for defineCommonConfig from react-microfrontend-common to vite-microfrontend

--- pages/front/vite.config.ts
+++ pages/front/vite.config.ts
@@ -4,7 +4,7 @@ import { federation } from '@module-federation/vite';
 import react from '@vitejs/plugin-react';
 
 import packageJson from './package.json' with { type: 'json' };
-import { defineCommonConfig } from 'react-microfrontend-common/vite';
+import { defineCommonConfig } from 'vite-microfrontend/vite';
 
 export default defineConfig(({ mode }) => {
 	const { remotes, base } = defineCommonConfig(mode);
