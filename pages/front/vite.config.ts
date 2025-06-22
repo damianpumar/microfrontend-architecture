c2df8ed0fedc605b359a5a366650b632e1ec447e
Damián Pumar <damianpumar@gmail.com>
2025-06-22 17:00:18 +0200

    fix: reorder import statements in vite.config.ts for consistency

--- pages/front/vite.config.ts
+++ pages/front/vite.config.ts
@@ -1,6 +1,5 @@
-import react from '@vitejs/plugin-react';
-
 import { defineConfig } from 'vite-microfrontend';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
 	federation: {
